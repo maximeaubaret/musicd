4982552e04af56e7e039421a39c6d707bed0e2ce
Maxime Aubaret <maxime.aubaret@gmail.com>
2026-02-23 00:12:32 +0100

    chore: prepare release v2.0.0

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -12,4 +12,4 @@ export const XDG_DATA_DIR = "musicd";
 export const XDG_AUTH_FILE = "auth.json";
 export const XDG_QUEUE_FILE = "queue.json";
 
-export const APP_VERSION = "1.2.2";
+export const APP_VERSION = "2.0.0";
